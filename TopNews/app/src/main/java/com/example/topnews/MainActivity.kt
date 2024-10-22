148d8d334fa016f17b5be405622e3b77481f8159
Gonki04 <goncaloaraujo2004@gmail.com>
2024-10-22 12:12:52 +0100

    TopNews- top bar and bottom bar

--- TopNews/app/src/main/java/com/example/topnews/MainActivity.kt
+++ TopNews/app/src/main/java/com/example/topnews/MainActivity.kt
@@ -4,30 +4,104 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.topnews.theme.TopNewsTheme
 import com.example.topnews.ui.ArticleDetail
 import com.example.topnews.ui.HomeView
+import com.example.topnews.ui.RowArticlePreview
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TopNewsTheme {
                 var navController = rememberNavController()
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    NavHost(navController = navController,
-                        startDestination = Screen.Home.route ) {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            colors = topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                titleContentColor = MaterialTheme.colorScheme.background,
+                            ),
+                            title = {
+                                Text(
+                                    text = "",
+                                    color = MaterialTheme.colorScheme.background
+                                )
+                            },
+                            navigationIcon = {
+                                IconButton(onClick = { navController.navigate(Screen.Home.route) }) {
+                                    Icon(Icons.Filled.Home, contentDescription = "Home")
+                                }
+                            },
+                            actions = {
+                                var horizontalArrangement = Arrangement.End
+                                IconButton(onClick = { /* Action for icon button */ }) {
+                                    Icon(Icons.Filled.Share, contentDescription = "Share")
+                                }
+                            }
+                        )
+                    },
+                    bottomBar = {
+                        BottomAppBar(
+                            containerColor = MaterialTheme.colorScheme.background,
+                            contentColor = MaterialTheme.colorScheme.background,
+                        ) {
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceAround
+                            ) {
+                                Button(onClick = { navController.navigate(Screen.Home.route)},
+                                    colors = ButtonDefaults.buttonColors(Color.LightGray))
+                                {
+                                    Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+                                }
+                                Button(onClick = { navController.navigate(Screen.ArticleDetail.route) },
+                                    colors = ButtonDefaults.buttonColors(Color.LightGray)) {
+                                    Icon(Icons.Filled.ArrowForward, contentDescription = "Forward")
+                                }
+                            }
+                        }
+                    },
+                ) { innerPadding ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.Home.route
+                    ) {
                         composable(route = Screen.Home.route) {
                             HomeView(
                                 navController = navController,
@@ -51,5 +125,4 @@ class MainActivity : ComponentActivity() {
 sealed class Screen(val route: String) {
     object Home : Screen("home")
     object ArticleDetail : Screen("article_detail/{articleUrl}")
-
 }
